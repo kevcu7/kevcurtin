1912ea893118d90ab75d37c5a64402c58e6fb800
Keshav <20260538@student.curtin.edu.au>
2020-04-09 13:37:22 +0400

    rename_vars changes updated

--- Maze.py
+++ Maze.py
@@ -65,14 +65,14 @@ if __name__ == "__main__":
             print("You fall into the chasm of doom.") # Out of bounds.
             
         else:
-            if grid[currentRow][currentCol] == EMPTY:
-                grid[currentRow][currentCol] = VISITED
+            if grid[row][col] == EMPTY:
+                grid[row][col] = VISITED
                 
-            elif grid[currentRow][currentCol] == WALL:
+            elif grid[row][col] == WALL:
                 done = True
                 print("You stumble blindly into a solid concrete wall.") # Hit wall.
 
-            elif grid[currentRow][currentCol] == END:
+            elif grid[row][col] == END:
                 done = True
                 solved = True
                 print("SOLVED!") # Solved.
