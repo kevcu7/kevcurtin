8fcffe726a888c1f01eb5aaf1561bd5d0d340a63
Keshav <20260538@student.curtin.edu.au>
2020-04-09 13:33:43 +0400

    if_replace changes updated

--- Maze.py
+++ Maze.py
@@ -65,20 +65,21 @@ if __name__ == "__main__":
             print("You fall into the chasm of doom.") # Out of bounds.
             
         else:
-            if grid[currentRow][currentCol] == EMPTY:
-                grid[currentRow][currentCol] = VISITED
-                
-            elif grid[currentRow][currentCol] == WALL:
-                done = True
-                print("You stumble blindly into a solid concrete wall.") # Hit wall.
+         cell = grid[currentRow][currentCol]
+	 if cell == EMPTY:
+		grid[currentRow][currentCol] = VISITED
 
-            elif grid[currentRow][currentCol] == END:
-                done = True
-                solved = True
-                print("SOLVED!") # Solved.
-                
-            else:
-                pass # Do nothing
+	 elif cell == WALL:
+		done = True
+		print("You stumble blindly into a solid concrete wall.")
+
+	 elif cell == END:
+		done = True
+		solved = True
+		print("SOLVED!")
+
+	 else:
+		pass # Do Nothing
         
         charIndex += 1
     # end-while
